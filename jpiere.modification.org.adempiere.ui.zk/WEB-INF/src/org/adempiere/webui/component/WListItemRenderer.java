bbc1672ccfce718f05a7c16914e2587b57fb484c
OES-MAIN-PC\hhagi <h.hagiwara@compiere-distribution-lab.net>
2025-02-25 23:06:37 +0900

    Maintain WListItemRenderer to release-12

--- jpiere.modification.org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/component/WListItemRenderer.java
+++ jpiere.modification.org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/component/WListItemRenderer.java
@@ -50,12 +50,15 @@ import org.compiere.util.DisplayType;
 import org.compiere.util.Env;
 import org.compiere.util.Language;
 import org.compiere.util.MSort;
+import org.compiere.util.Msg;
 import org.compiere.util.Util;
+import org.compiere.util.ValueNamePair;
 import org.zkoss.image.AImage;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.EventListener;
 import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zul.Decimalbox;
 import org.zkoss.zul.Html;
 import org.zkoss.zul.Image;
@@ -112,7 +115,9 @@ public class WListItemRenderer implements ListitemRenderer<Object>, EventListene
 		for (String columnName : columnNames)
 		{
 			tableColumn = new WTableColumn();
-			tableColumn.setHeaderValue(Util.cleanAmp(columnName));
+			ValueNamePair msgTipPair = Msg.splitToMsgTip(columnName);
+			tableColumn.setHeaderValue(Util.cleanAmp(msgTipPair.getValue()));
+			tableColumn.setTooltipText(Util.cleanAmp(msgTipPair.getName()));
 			m_tableColumns.add(tableColumn);
 		}
 	}
@@ -213,7 +218,7 @@ public class WListItemRenderer implements ListitemRenderer<Object>, EventListene
 		if (m_tableColumns.size() > columnIndex) {
 			column = getColumn(columnIndex);
 			if (column != null && column.getHeaderValue() != null) {
-				listcell.setWidgetAttribute(AdempiereWebUI.WIDGET_INSTANCE_NAME, column.getHeaderValue().toString());
+				listcell.setClientAttribute(AdempiereWebUI.WIDGET_INSTANCE_NAME, column.getHeaderValue().toString());
 			}
 		}
 		boolean isCellEditable = table != null ? table.isCellEditable(rowIndex, columnIndex) : false;
@@ -365,7 +370,6 @@ public class WListItemRenderer implements ListitemRenderer<Object>, EventListene
 
 						listcell.setLabel(format.format(value));
 						ZkCssHelper.appendStyle(listcell, "text-align: right");
-
 					}
 				}
 			}
@@ -880,7 +884,7 @@ public class WListItemRenderer implements ListitemRenderer<Object>, EventListene
 	}
 
 	/**
-	 * Reset the renderer.
+	 * Reset the renderer.<br/>
 	 * This should be called if the table using this renderer is cleared.
 	 */
 	public void clearColumns()
@@ -1015,6 +1019,7 @@ public class WListItemRenderer implements ListitemRenderer<Object>, EventListene
 			if (listBox != null && Events.ON_DOUBLE_CLICK.equals(event.getName())) {
 				Event evt = new Event(Events.ON_DOUBLE_CLICK, listBox);
 				Events.sendEvent(listBox, evt);
+				Clients.evalJavaScript("jq('img.fullsize-image').remove();");
 			}
 		}
 
