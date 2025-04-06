6ddc0af0fa4ed71fa9f377bf2f43341eed107b4a
OES-MAIN-PC\hhagi <h.hagiwara@compiere-distribution-lab.net>
2025-04-06 13:15:49 +0900

    Maintain to IDEMPIERE-5275 Tabular Report Re-Run button/close parameter window

--- jpiere.modification.org.adempiere.ui.zk/WEB-INF/src/jpiere/modification/webui/window/JPiereZkReportViewerProvider.java
+++ jpiere.modification.org.adempiere.ui.zk/WEB-INF/src/jpiere/modification/webui/window/JPiereZkReportViewerProvider.java
@@ -14,11 +14,14 @@
 package jpiere.modification.webui.window;
 
 import org.adempiere.webui.apps.AEnv;
+import org.adempiere.webui.apps.ProcessDialog;
 import org.adempiere.webui.component.Window;
 import org.adempiere.webui.part.WindowContainer;
 import org.adempiere.webui.session.SessionManager;
 import org.compiere.print.ReportEngine;
 import org.compiere.print.ReportViewerProvider;
+import org.compiere.util.Env;
+import org.compiere.util.Util;
 import org.zkoss.zk.ui.Executions;
 
 /**
@@ -47,15 +50,25 @@ public class JPiereZkReportViewerProvider implements ReportViewerProvider {
 		}
 	}
 	
+	/**
+	 * Open report viewer
+	 * @param report
+	 */
 	protected void openReportViewWindow (ReportEngine report) {
-		Window viewer = new JPiereZkReportViewer(report, report.getName());
+		Window viewer = new JPiereZkReportViewer(report, report.getName());//JPIERE-0598: JPiere Attachment File at Report
 		
-    	viewer.setAttribute(Window.MODE_KEY, Window.MODE_EMBEDDED);
-    	viewer.setAttribute(Window.INSERT_POSITION_KEY, Window.INSERT_NEXT);
-    	if(report.isReplaceTabContent())
-    		viewer.setAttribute(Window.INSERT_POSITION_KEY, Window.REPLACE);
-    	
-    	viewer.setAttribute(WindowContainer.DEFER_SET_SELECTED_TAB, Boolean.TRUE);
-    	SessionManager.getAppDesktop().showWindow(viewer);
+		viewer.setAttribute(Window.MODE_KEY, Window.MODE_EMBEDDED);
+		viewer.setAttribute(Window.INSERT_POSITION_KEY, Window.INSERT_NEXT);
+		if(report.isReplaceTabContent()) {
+			viewer.setAttribute(Window.INSERT_POSITION_KEY, Window.REPLACE);
+			viewer.setAttribute(WindowContainer.REPLACE_WINDOW_NO, report.getWindowNo());
+			String predefined = Env.getContext(Env.getCtx(), report.getWindowNo(), ProcessDialog.SAVED_PREDEFINED_CONTEXT_VARIABLES);
+			if (!Util.isEmpty(predefined, true)) {
+				viewer.setAttribute(ProcessDialog.SAVED_PREDEFINED_CONTEXT_VARIABLES, predefined);
+			}
+			viewer.setAttribute("IsSOTrx", Env.getContext(Env.getCtx(), report.getWindowNo(), "IsSOTrx"));
+		}
+		viewer.setAttribute(WindowContainer.DEFER_SET_SELECTED_TAB, Boolean.TRUE);
+		SessionManager.getAppDesktop().showWindow(viewer);
 	}
 }
