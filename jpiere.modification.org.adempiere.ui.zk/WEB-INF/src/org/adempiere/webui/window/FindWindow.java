c1a98a873369440a321b53a8d8df7dd5ddedb1b4
OES-MAIN-PC\hhagi <h.hagiwara@compiere-distribution-lab.net>
2026-02-11 14:41:40 +0900

    Maintain to IDEMPIERE-6738 Problem in Find Dialog

--- jpiere.modification.org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/window/FindWindow.java
+++ jpiere.modification.org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/window/FindWindow.java
@@ -20,10 +20,10 @@ package org.adempiere.webui.window;
 import static org.compiere.model.SystemIDs.*;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -2423,24 +2423,66 @@ public class FindWindow extends Window implements EventListener<Event>, ValueCha
 
         //  Execute Qusery
         m_total = 999999;
-        Statement stmt = null;
+        PreparedStatement pstmt = null;
         ResultSet rs = null;
+        Connection conn = null;
         try
         {
-            stmt = DB.createStatement();
-            rs = stmt.executeQuery(finalSQL);
+        	pstmt = DB.prepareStatement(finalSQL, null);
+        	conn =  pstmt.getConnection();
+        	conn.setAutoCommit(false);
+        	conn.setReadOnly(true);
+            rs = pstmt.executeQuery();
             if (rs.next())
                 m_total = rs.getInt(1);
         }
         catch (SQLException e)
         {
+
+    		if (conn != null)
+    		{
+    			try {
+					conn.rollback();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+    		}
             log.log(Level.SEVERE, finalSQL, e);
         }
         finally
         {
-        	DB.close(rs, stmt);
+        	DB.close(rs, pstmt);
         	rs = null;
-        	stmt = null;
+        	pstmt = null;
+
+        	if (conn != null)
+        	{
+        		boolean isClosedconn = false;
+	    		try {
+	    			isClosedconn = conn.isClosed();
+	    		} catch (SQLException e) {
+	    			e.printStackTrace();
+	    		}
+
+	    		if(!isClosedconn)
+	    		{
+		    		try {
+		    			conn.setAutoCommit(true);
+		    		} catch (SQLException e) {
+		    			e.printStackTrace();
+		    		}
+		    		try {
+		    			conn.setReadOnly(false);
+		    		} catch (SQLException e) {
+		    			e.printStackTrace();
+		    		}
+		    		try {
+		    			conn.close();
+		    		} catch (SQLException e) {
+		    			e.printStackTrace();
+		    		}
+	    		}
+        	}
         }
         //  No Records
       /*  if (m_total == 0 && alertZeroRecords)
