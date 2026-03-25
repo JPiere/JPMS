4116a1d90e3b48c73092d72eb13a18a3afafc655
OES-MAIN-PC\hhagi <h.hagiwara@compiere-distribution-lab.net>
2026-03-25 12:14:04 +0900

    IDEMPIERE-6844 Prevent CSV Injection in iDempiere WebUI

--- jpiere.modification.org.adempiere.base/src/org/idempiere/print/renderer/DelimitedReportRenderer.java
+++ jpiere.modification.org.adempiere.base/src/org/idempiere/print/renderer/DelimitedReportRenderer.java
@@ -29,7 +29,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 
-import org.compiere.model.MLocation;
+import org.compiere.model.MSysConfig;
 import org.compiere.print.MPrintFormat;
 import org.compiere.print.MPrintFormatItem;
 import org.compiere.print.PrintData;
@@ -45,7 +45,7 @@ import org.compiere.util.FragmentDisplayType;
 import org.compiere.util.Ini;
 import org.compiere.util.KeyNamePair;
 import org.compiere.util.Language;
-import org.compiere.util.NamePair;
+import org.compiere.util.Util;
 
 /**
  * Abstract base class for renderer that output delimited text content.
@@ -260,6 +260,8 @@ public abstract class DelimitedReportRenderer<C extends DelimitedReportRendererC
 		if (content == null || content.length() == 0)
 			return;
 		//
+		if (MSysConfig.getBooleanValue(MSysConfig.CSV_EXPORT_SANITIZATION, true, Env.getAD_Client_ID(Env.getCtx())))
+			content = Util.sanitizeCsvValue(content);
 		boolean needMask = false;
 		StringBuilder buff = new StringBuilder();
 		char chars[] = content.toCharArray();
